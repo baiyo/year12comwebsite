c5b356627fa61f0af05465ec64095eb541c2745c
baiyo <baophan12144@gmail.com>
2024-02-26 09:40:32 +1300

    asddasfgsdfggdt

--- files/js/app.js
+++ files/js/app.js
@@ -49,3 +49,12 @@ VanillaTilt.init(document.querySelectorAll(".data-tilt"), {
     max: 15,
     speed: 400
 });
+
+function ScrollIntoFrame(id){
+    // let section = document.querySelectorAll(".section-".concat(wut));
+
+
+    document.getElementById(id).scrollIntoView();
+    // ('section#' + id)[0].scrollIntoView();
+    // section.scrollIntoView({behavior: 'smooth'})
+}
